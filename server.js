c7b06a19d4ced7089f10461d70499cf404ecea1c
Alex Herrera <vamihero01@gmail.com>
2026-08-10 14:53:38 -0500

    Agrega middleware de logging

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const logger = require('./src/middleware/logger');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(logger);
 
 app.get('/', (req, res) => {
     res.json({
