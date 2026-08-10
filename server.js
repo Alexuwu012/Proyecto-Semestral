b363d279a0a488429886809e8f8c69c3abe6552a
Alex Herrera <vamihero01@gmail.com>
2026-08-10 14:40:06 -0500

    Configuración inicial del proyecto Alerta Segura

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello API'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
